120183a266ae5c824a190c39ab569e6a0f36e7ae
= <adam.brutsaert@epitech.eu>
2022-10-04 15:15:23 +0200

    Added header parsing

--- src/main.c
+++ src/main.c
@@ -7,15 +7,15 @@
 
 int main()
 {
-    /* toml_map_t *map = toml_parse_file("res/config.toml");
-    toml_map_destroy(map); */
+    toml_map_t *map = toml_parse_file("res/config.toml");
+    toml_map_destroy(map);
 
     /* toml_token_t token = toml_token_create_string(toml_strdup("true"), 0, 0);
     
     toml_boolean_t result = toml_parser_parse_boolean(token);
     printf("Result: >>>%s<<<\n", result ? "true" : "false");
 
-    toml_token_destroy(token); */
+    toml_token_destroy(token);
 
     toml_map_t *map = toml_map_create();
 
@@ -24,7 +24,7 @@ int main()
     printf("'test' exists : %d\n", toml_map_exists(map, "test"));
     printf("'key' exists : %d\n", toml_map_exists(map, "key"));
 
-    toml_map_destroy(map);
+    toml_map_destroy(map); */
 
     return 0;
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -4,7 +4,38 @@
 
 static void parse_header(toml_parser_t *parser)
 {
-    
+    // TODO check whenever I do next if we reached end or if it is the expected token
+    toml_token_destroy(toml_tokenizer_next(parser->tokenizer));
+
+    toml_token_t token = toml_tokenizer_next(parser->tokenizer);
+    toml_string_t string = toml_parser_parse_string(token);
+    toml_token_destroy(token);
+
+    if (!(toml_map_exists(parser->current, string) && toml_map_get_type(parser->current, string) == TOML_TYPE_MAP))
+        toml_map_set_map(parser->current, string, toml_map_create());
+
+    parser->current = toml_map_get_map(parser->current, string);
+
+    while (toml_tokenizer_peek(parser->tokenizer, 0).type == TOML_TOKEN_TYPE_DOT) {
+        toml_token_destroy(toml_tokenizer_next(parser->tokenizer));
+
+        token = toml_tokenizer_next(parser->tokenizer);
+        string = toml_parser_parse_string(token);
+        toml_token_destroy(token);
+
+        if (!(toml_map_exists(parser->current, string) && toml_map_get_type(parser->current, string) == TOML_TYPE_MAP))
+            toml_map_set_map(parser->current, string, toml_map_create());
+
+        parser->current = toml_map_get_map(parser->current, string);
+    }
+
+    while (!toml_tokenizer_end(parser->tokenizer)) {
+        toml_token_type_t type = toml_tokenizer_peek(parser->tokenizer, 0).type; 
+        toml_token_destroy(toml_tokenizer_next(parser->tokenizer));
+
+        if (type == TOML_TOKEN_TYPE_NEWLINE)
+            break;
+    }
 }
 
 toml_map_t *toml_parser_parse_file(char const *filename)
@@ -17,9 +48,12 @@ toml_map_t *toml_parser_parse_file(char const *filename)
     parser.current = parser.root;
     
     while (!toml_tokenizer_end(parser.tokenizer)) {
-        toml_token_t token = toml_tokenizer_next(parser.tokenizer);
-        toml_token_print(token);
-        toml_token_destroy(token);
+        toml_token_t token = toml_tokenizer_peek(parser.tokenizer, 0);
+        
+        if (token.type == TOML_TOKEN_TYPE_LBRACKET)
+            parse_header(&parser);
+        else
+            toml_token_destroy(toml_tokenizer_next(parser.tokenizer));
     }
     
     toml_tokenizer_destroy(parser.tokenizer);
